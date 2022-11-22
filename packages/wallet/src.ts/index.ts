2ea79a613df148cb32e31d71373763722e170905
sarahzdev <sarah.hg.zhang@gmail.com>
2022-11-22 15:15:30 -0800

    use estimateGas to set gas limit

--- packages/wallet/src.ts/index.ts
+++ packages/wallet/src.ts/index.ts
@@ -141,7 +141,7 @@ export class PKPWallet extends Signer implements ExternallyOwnedAccount, TypedDa
         }
 
         if ( ! transaction['gasLimit'] ) {
-            transaction.gasLimit = 150000;
+            transaction.gasLimit = await this.rpcProvider.estimateGas(transaction);
         }
 
         return resolveProperties(transaction).then(async (tx) => {
@@ -197,7 +197,7 @@ export class PKPWallet extends Signer implements ExternallyOwnedAccount, TypedDa
             return this.provider.resolveName(name);
         });
 
-        return joinSignature(this._signingKey().signDigest(_TypedDataEncoder.hash(populated.domain, types, populated.value)));
+        // return joinSignature(this._signingKey().signDigest(_TypedDataEncoder.hash(populated.domain, types, populated.value)));
     }
 
     encrypt(password: Bytes | string, options?: any, progressCallback?: ProgressCallback): Promise<string> {
